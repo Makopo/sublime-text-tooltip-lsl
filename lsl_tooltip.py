f08934edfc7623d5c2b492c85b091ecd5797b49b
Makopoppo <bokemako@gmail.com>
2016-06-13 00:16:09 +0900

    Now no longer need previous implemention

--- lsl_tooltip.py
+++ lsl_tooltip.py
@@ -2,7 +2,6 @@ import sublime, sublime_plugin
 import os
 import json
 import webbrowser
-# import xml.etree.ElementTree as etree
 
 class LslTooltipListener(sublime_plugin.EventListener):
 
@@ -23,45 +22,6 @@ class LslTooltipCommand(sublime_plugin.TextCommand):
         # select word
         word = self.view.substr(self.view.word(self.view.sel()[0]))
 
-        # I temporarily borrow the logic from @buildersbrewery here for PoC - it will be replaced later on
-        # try:
-        #     tooltipRows = []
-        #     for result in KWDB.findall(".//*[@name='" + word + "']"):
-        #         if result.tag == 'param':
-        #             continue
-        #         if result.tag == 'function' or result.tag == 'constant':
-        #             tooltipRows.append('### (%s) <a href="https://wiki.secondlife.com/w/index.php?title=Special:Search&go=Go&search=%s">%s</a>' % (result.get('type', 'void'), result.get('name'), result.get('name')))
-        #         else:
-        #             tooltipRows.append('### <a href="https://wiki.secondlife.com/w/index.php?title=Special:Search&go=Go&search=%s">%s</a>' % (result.get('name'), result.get('name')))
-        #         if result.tag == 'constant':
-        #             tooltipRows.append(' ')
-        #             tooltipRows.append('**Value**: %s' % str(result.get('value')))
-        #         if result.get('status', None) is not None and result.get('status', 'normal') != 'normal':
-        #             tooltipRows.append(' ')
-        #             tooltipRows.append('<body style="color:#fff;background-color:#820124;">**Status**: %s</body>' % result.get('status', 'normal'))
-        #         if result.get('delay', None) is not None:
-        #             tooltipRows.append(' ')
-        #             tooltipRows.append('**Delay**: %s' % str(result.get('delay')))
-        #         if result.get('energy', None) is not None:
-        #             tooltipRows.append(' ')
-        #             tooltipRows.append('**Energy**: %s' % str(result.get('energy')))
-        #         if result.tag == 'function' or result.tag == 'event':
-        #             if result.findall('./param') != []:
-        #                 tooltipRows.append(' ')
-        #                 tooltipRows.append('#### Parameters')
-        #                 for param in result.iter('param'):
-        #                     tooltipRows.append('* (%s) **%s**' % (param.get('type'), param.get('name')))
-        #         if result.find('description').text is not None:
-        #             tooltipRows.append(' ')
-        #             tooltipRows.append('#### Description')
-        #             tooltipRows.append(' ')
-        #             tooltipRows.append('%s' % result.find('description').text.strip())
-
-        #     print('\n'.join(tooltipRows))
-
-        # except Exception as e:
-        #     print(e)
-
         # Fixed string for now lol
         self.view.show_popup(TooltipData[word], location=-1, max_width=600, max_height=350, on_navigate=self.on_navigate)
 
@@ -78,7 +38,6 @@ def plugin_loaded():
 
     class Pref:
         def load(self):
-            Pref.debug = False
             Pref.isActive = False
             Pref.word = ""
     Pref = Pref()
@@ -87,6 +46,3 @@ def plugin_loaded():
     with open(os.path.join(sublime.packages_path(), 'TooltipLSL', 'tooltipdata.json')) as data_file:    
         TooltipData = json.load(data_file)
 
- #    kwdbElementTree = etree.parse(os.path.join(sublime.packages_path(), 'TooltipLSL', 'kwdb.xml'))
- #    KWDB = kwdbElementTree.getroot()
- # 
